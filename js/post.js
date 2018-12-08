cfffee4b3b33d83dfef289e66f2631ca0a7fa040
SoonYub Hwang <nowonbun@naver.com>
2018-12-09 02:24:08 +0900

    20181207 The post was updated.

--- js/post.js
+++ js/post.js
@@ -7,15 +7,15 @@ var post = function (obj) {
         $("img").on("click", function () {
             $("#ImgModal").show();
             $("#ImgModal .modal-content").attr("src", this.src);
-            $("body").css("position","fixed");
+            //$("body").css("position","fixed");
         });
         $("#ImgModal").on("click", function () {
             $("body").css("position", "");
-            $("#ImgModal").hide();
+            //$("#ImgModal").hide();
         });
         $("#ImgModal .close").on("click", function () {
             $("body").css("position", "");
-            $("#ImgModal").hide();
+            //$("#ImgModal").hide();
         });
     }
 });
