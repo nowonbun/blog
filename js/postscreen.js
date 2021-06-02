354e8f29e60b94638601ae4e5948d2a996295561
nowonbun <nowonbun@naver.com>
2021-06-02 17:22:35 +0900

    2021/06/02 17:22:32

--- js/postscreen.js
+++ js/postscreen.js
@@ -6,6 +6,7 @@ var _this = (function(obj) {
 	__.fn = {}
 	__.ev = function() {
 		$("pre code.hljs").each(function () {
+			$(this).before($("<a href='javascript:void(0);' class='code-copy'><i class='fa fa-copy'></i>Copy!</a>"));
             $(this).before($("<div class='code-title'></div>").append($("<i class='fa fa-minus-square code-collapse'></i>"))
                 .append("&nbsp;[Source view]&nbsp;" + $(this).data("type")));
             $(this).parent().addClass("code-view");
@@ -22,6 +23,31 @@ var _this = (function(obj) {
                 $i.addClass("fa-plus-square");
                 $this.parent().addClass("code-view-disabled");
             }
+        });
+        $(document).on("click",'.code-copy', function(){
+            $parent = $(this).closest(".code-view");
+            if($parent.hasClass("code-view-disabled")){
+                $i = $parent.find("i.code-collapse");
+                $i.removeClass("fa-plus-square");
+                $i.addClass("fa-minus-square");
+                $parent.removeClass("code-view-disabled");
+            }
+            toastr.success(null,"コピーされました。", {timeOut: 700});
+            var code_element = $(this).closest("pre").find("code")[0];
+            var value = code_element.innerText.replace(/\n\n\n/ig, '').replace('    \n','');
+            var selection = window.getSelection();
+            var body_element = document.getElementsByTagName('body')[0];
+            var newdiv = document.createElement('div');
+            newdiv.style.position = 'absolute';
+            newdiv.style.left = '-10000px';
+            newdiv.style.top = '-10000px';
+            body_element.appendChild(newdiv);
+            newdiv.innerHTML = "<pre>" + escapeHTML(value) + "</pre>";
+            selection.selectAllChildren(newdiv);
+            setTimeout(function(){
+                newdiv.remove();
+            },10000);
+            document.execCommand('copy'); 
         });
 	}
 	$(__.ev);
